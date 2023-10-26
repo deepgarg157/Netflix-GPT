3f4cf7fb2e93a1ee1a6a7df0a77318e90533042e
unknown <deepgarg157@gmail.com>
2023-10-26 09:16:12 +0530

    Netflix GPT

---
+++ src/Components/Body.js
@@ -0,0 +1,24 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./Login";
+import Browse from "./Browse";
+
+const appLayout = createBrowserRouter([
+    {
+        path: "/",
+        element: <Login />
+    },
+    {
+        path: "/browse",
+        element: <Browse />
+    }
+])
+
+const Body = () => {
+    return (
+        <div>
+            <RouterProvider router={appLayout} />
+        </div>
+    )
+}
+
+export default Body;
---
+++ src/Components/Header.js
@@ -0,0 +1,9 @@
+const Header =()=>{
+    return(
+        <div>
+            Header Page;
+        </div>
+    )
+}
+
+export default Header;
