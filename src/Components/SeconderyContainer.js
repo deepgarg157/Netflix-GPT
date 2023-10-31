329c863f2f8226149b8d40ec351912ed04bd917e
unknown <deepgarg157@gmail.com>
2023-10-31 10:34:18 +0530

    Netflix GPT

--- src/Components/SeconderyContainer.js
+++ src/Components/SeconderyContainer.js
@@ -9,9 +9,9 @@ const SecondaryContainer = () => {
         <div className="bg-black">
             <div className="-mt-48 relative z-10">
                 <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
-                <MovieList title={"Trending"} movies={movies.nowPlayingMovies} />
-                <MovieList title={"Up Coming"} movies={movies.nowPlayingMovies} />
-                <MovieList title={"Popular"} movies={movies.nowPlayingMovies} />
+                <MovieList title={"Top Rated"} movies={movies.topRatedMovies} />
+                <MovieList title={"Up Coming"} movies={movies.upComingMovies} />
+                <MovieList title={"Popular"} movies={movies.popularMovies} />
                 <MovieList title={"Horror"} movies={movies.nowPlayingMovies} />
             </div>
         </div>
---
+++ src/hooks/useUpComingMovies.js
@@ -0,0 +1,27 @@
+
+
+import { useDispatch } from "react-redux";
+import { API_OPTIONS } from "../Utility/constants";
+import { addUpComingMovies } from "../Utility/moviesSlice";
+import { useEffect } from "react";
+
+const useUpComingMovies = ()=>{
+    
+    const dispatch = useDispatch()
+
+    const getUpComingMovies = async () => {
+        const data = await fetch(
+            "https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1",
+            API_OPTIONS
+        );
+        const json = await data.json();
+        // console.log(json.results)
+        dispatch(addUpComingMovies(json.results))
+    }
+
+    useEffect(()=>{
+        getUpComingMovies()
+    },[])
+}
+
+export default useUpComingMovies;
